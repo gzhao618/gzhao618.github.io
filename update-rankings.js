5da7ee4c145ed6becd756a2addbb360ce9680662
will-r-wang <william.wang@shopify.com>
2020-08-15 15:06:30 -0400

    Restore overallrankings.html page
    Now that we've verified the Github workflow works as intended, this
    configures the update-rankings.js file to restore the page back to its
    original state. The next run at ~4pm will create the bot commit.
    
    Once the Google spreadsheet information is acquired, we can configure
    more of the code so that it displays real-time updates.

--- update-rankings.js
+++ update-rankings.js
@@ -1,1 +1,274 @@
-console.log("Testing workflow. This should replace everything in overallranking.html with this string.");
+let output = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link 	rel="icon" 
+      		type="image/png" 
+      		href="https://www.cheo.on.ca/en/images/structure/news_avatar.jpg">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
+	<title> CHEO-OPOLY!</title>
+
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<style>
+	tr:nth-child(even) {background-color: #f2f2f2;}
+
+	table {
+ 		 border-collapse: collapse;
+  			width: 100%;
+		}
+
+	th, td {
+  		text-align: left;
+  		padding: 20px;
+		}
+	</style>
+</head>
+
+<body style="overflow-x: hidden;">
+
+	<div class="container">
+		<div class="nav-wrapper">
+			<div class="left-side">
+				<div class="nav-logo">
+					<a href="https://www.cheo.on.ca/en/index.aspx"><img src="images/logo.jpg" alt="CHEO Logo"></a>
+				</div>
+				<div class="nav-link-wrap">
+					<a href="index.html"><p>Home</p></a>
+				</div>
+				<div class="nav-link-wrap">
+					<div class="dropdown">
+					<a href="page1.html"><p>Auction</p></a>
+					<div class="dropdown-content">
+						<a href="page1.html">Projects</a>
+						<a href="misc.html">Miscellaneous</a>
+  					</div>
+					</div>
+					
+				</div>
+				<div class="nav-link-wrap">
+					<div class="dropdown">
+					<a href="overallranking.html"><p>Leaderboards</p></a>
+					<div class="dropdown-content">
+						<a href="overallranking.html">Overall</a>
+						<a href="projects1.html">Projects</a>
+  						<a href="miscboard.html">Misc</a>
+  					</div>
+					</div>
+				</div>
+				<div class="nav-link-wrap">
+					<a href="rules.html"><p>Rules</p></a>
+				</div>
+			</div>
+
+			<div class="right-side">
+				<div><p style="font-size:1.1vw;">CHEO-OPOLY</p></div>
+				<div class="nav-link-bid">
+					<a href="page1.html">BID NOW</a>
+				</div>
+			</div>
+		</div>
+
+
+
+	<div class="limiter-left">
+		<div class="container-table100">
+			<div class="wrap-table100">
+			<h1>Project Rankings</h1>
+				<div class="table100 ver1 m-b-110">
+					<div class="table100-head">
+						<table>
+							<thead>
+								<tr class="row100 head">
+									<th class="cell100 column1">Ranking</th>
+									<th class="cell100 column2">Project</th>
+									<th class="cell100 column3">Contributed Amount</th>
+									
+								</tr>
+							</thead>
+						</table>
+					</div>
+
+					<div class="table100-body js-pscroll">
+						
+						<table>
+							<tbody>
+								
+<tr class="row100 body">
+<td class="cell100 column1">1</td>
+<td class="cell100 column2">CompanyABC</td>
+<td class="cell100 column3">$5000</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">2</td>
+<td class="cell100 column2">CompanyDEF</td>
+<td class="cell100 column3">$4000</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">3</td>
+<td class="cell100 column2">CompanyGHI</td>
+<td class="cell100 column3">$3000</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">4</td>
+<td class="cell100 column2">CompanyJKL</td>
+<td class="cell100 column3">$2750</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">5</td>
+<td class="cell100 column2">CompanyMNO</td>
+<td class="cell100 column3">$2500</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">6</td>
+<td class="cell100 column2">CompanyPQR</td>
+<td class="cell100 column3">$2250</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">7</td>
+<td class="cell100 column2">CompanySTU</td>
+<td class="cell100 column3">$2000</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">8</td>
+<td class="cell100 column2">CompanyVWX</td>
+<td class="cell100 column3">$1750</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">9</td>
+<td class="cell100 column2">CompanyXYZ</td>
+<td class="cell100 column3">$1500</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">10</td>
+<td class="cell100 column2">ABCEnterprises</td>
+<td class="cell100 column3">$1250</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">11</td>
+<td class="cell100 column2">DEFEnterprises</td>
+<td class="cell100 column3">$1000</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">12</td>
+<td class="cell100 column2">GHIEnterprises</td>
+<td class="cell100 column3">$900</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">13</td>
+<td class="cell100 column2">JKLEnterprises</td>
+<td class="cell100 column3">$800</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">14</td>
+<td class="cell100 column2">MNOEnterprises</td>
+<td class="cell100 column3">$700</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">15</td>
+<td class="cell100 column2">PQREnterprises</td>
+<td class="cell100 column3">$600</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">16</td>
+<td class="cell100 column2">STUEnterprises</td>
+<td class="cell100 column3">$500</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">17</td>
+<td class="cell100 column2">VWXEnterprises</td>
+<td class="cell100 column3">$400</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">18</td>
+<td class="cell100 column2">XYZEnterprises</td>
+<td class="cell100 column3">$300</td>
+</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+	<div class="limiter-right">
+		<div class="container-table100">
+			<div class="wrap-table100">
+			<h1>Highest Bidder</h1>
+				<div class="table100 ver1 m-b-110">
+					<div class="table100-head">
+						<table>
+							<thead>
+								<tr class="row100 head">
+									<th class="cell100 column1">Ranking</th>
+									<th class="cell100 column2">Company</th>
+									<th class="cell100 column3">Contributed Amount</th>
+									
+								</tr>
+							</thead>
+						</table>
+					</div>
+
+					<div class="table100-body js-pscroll">
+						<table>
+							<tbody>
+<tr class="row100 body">
+<td class="cell100 column1">1</td>
+<td class="cell100 column2">CompanyABC</td>
+<td class="cell100 column3">$5000</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">2</td>
+<td class="cell100 column2">CompanyDEF</td>
+<td class="cell100 column3">$4000</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">3</td>
+<td class="cell100 column2">CompanyGHI</td>
+<td class="cell100 column3">$3000</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">4</td>
+<td class="cell100 column2">CompanyJKL</td>
+<td class="cell100 column3">$2750</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">5</td>
+<td class="cell100 column2">CompanyMNO</td>
+<td class="cell100 column3">$2500</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">6</td>
+<td class="cell100 column2">CompanyPQR</td>
+<td class="cell100 column3">$2250</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">7</td>
+<td class="cell100 column2">CompanySTU</td>
+<td class="cell100 column3">$2000</td>
+</tr>
+<tr class="row100 body">
+<td class="cell100 column1">8</td>
+<td class="cell100 column2">CompanyVWX</td>
+<td class="cell100 column3">$1750</td>
+</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		<div class="footer">
+			<div class="socials">
+				<a href="https://www.facebook.com/CHEOkids/" class="fa fa-facebook" target="_blank"></a>
+				<a href="https://www.instagram.com/cheohospital/?hl=en" class="fa fa-instagram" target="_blank"></a>	
+				<a href="https://twitter.com/CHEO?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" class="fa fa-twitter" target="_blank"></a>	
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+`;
+
+console.log(output);
